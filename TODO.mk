4346792623e896ecb3a75c5eef402d5df26ad604
Enrico Pittini <pittinienrico@hotmail.it>
2022-05-08 13:15:09 +0200

    Update TODO.mk

--- TODO.mk
+++ TODO.mk
@@ -3,3 +3,7 @@
 - Try-except handling
 - Already existing output file handling
 - Refactor private functions
+- Check and clean visualization script
+
+# MiniZinc model 
+- Change solver change model
