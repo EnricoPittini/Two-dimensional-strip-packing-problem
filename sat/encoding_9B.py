726eb5ff4c0ac0679383b0f1c6225107a83fd7c5
Enrico Pittini <pittinienrico@hotmail.it>
2022-06-16 18:13:51 +0200

    Add encodings 9 (to refactor)

---
+++ sat/encoding_9B.py
@@ -0,0 +1,270 @@
+from z3 import *
+from sat_utils import at_least_one, at_most_one, exactly_one, UnsatError, Vlsi_sat_abstract
+# from math import ceil
+
+
+class Vlsi_sat(Vlsi_sat_abstract):
+    """Class for solving the VLSI problem in SAT, using the encoding 4A.
+
+    It inherits from the class `Vlsi_sat_abstract`.
+
+    Same solving and optimization of the encoding 3.
+    The only difference is the all the non-necessary constraints are removed.
+    (The non-necessary constraints are both in `__solve` and in `__process_solver_instance`).
+
+    """
+
+    def __init__(self, w, n, dims, results):
+        super().__init__(w, n, dims, results)
+
+
+    def __solve(self, w_min, h_min, l_min, l_max):
+        """Solves the given VLSI instance, using the SAT encoding 4A.
+
+        It is an auxiliary method. Its aim is to solve the VLSI instance without performing optimization: any solution is 
+        good.
+
+        Parameters
+        ----------
+        w_min : int
+            Minimum width of a circuit
+        h_min : int
+            Minimum height of a circuit
+        l_min : int
+            Lower bound of the length of the plate
+        l_max : int
+            Upper bound of the length of the plate
+
+        Returns
+        -------
+        s: z3.z3.Solver
+            The solver instance
+        coords : list of list of list of z3.z3.BoolRef
+            Boolean variables 'coord_i_j_k'.
+            See `Notes`.
+        lengths : list of list of z3.z3.BoolRef
+            Boolean variables 'length_k_l'.
+            See `Notes`.
+
+        Notes
+        ------
+        The following boolean variables are used
+        - circuit_i_j_k, where 'i' in [0,w-w_min+1], 'j' in [0,l_max-h_min+1], 'k' in [0,n]. 
+          '(i,j)' represent two coordinates of the plate, 'k' represents a circuit.
+          circuit_i_j_k is True IIF the circuit 'k' is present in the cell '(i,j)' of the plate.
+        - coord_i_j_k, where 'i' in [0,w-w_min+1], 'j' in [0,l_max-h_min+1], 'k' in [0,n].
+          '(i,j)' represent two coordinates of the plate, 'k' represents a circuit.
+          coord_i_j_k is True IIF the left-bottom corner of the circuit 'k' is put in the cell '(i,j)' of the plate.
+        - length_k_l, where 'k' in [0,n] and 'l' in [0,l_max-l_min+1].
+          'k' represents a circuit, 'l' represents a length of the plate.
+           length_k_l is True IIF the circuit 'k' uses the length 'l' of the plate.
+           For going from an index 'l' of 'length_k_l' to the actual length: l+l_min-1.
+           For going from an actual length 'l' to an index of 'length_k_l': l-l_min+1.
+
+        """
+        w, n, dimsX, dimsY = self.w, self.n, self.dimsX, self.dimsY
+
+        s = Solver()  # Solver instance
+
+        # List of lists of lists, containing the 'circuits' boolean variables: variables 'circuit_i_j_k'
+        circuits = [[[Bool(f'circuit_{i}_{j}_{k}') for k in range(n)] for j in range(l_max-h_min+1)] for i in range(w-w_min+1)]
+        # List of lists of lists, containing the 'coords' boolean variables: variables 'coord_i_j_k'
+        coords = [[[Bool(f'coord_{i}_{j}_{k}') for k in range(n)] for j in range(l_max-h_min+1)] for i in range(w-w_min+1)]
+        # List of lists of lists, containing the 'lengths' boolean variables: variables 'length_k_l'
+        lengths = [[Bool(f'length_{k}_{l}') for l in range(l_max-l_min+1)] for k in range(n)]
+                
+        # Constraint: the left-bottom corner of each circuit 'k' must be present exactly one time across the plate
+        for k in range(n):
+            s.add(exactly_one([coords[i][j][k] for i in range(w-w_min+1) for j in range(l_max-h_min+1)], name=f'exactly_one_{k}'))
+
+        # print('CUCU')  # TODO: remove
+
+        # Constraint: for each circuit 'k' and for each cell '(i,j)' of the plate, if that cell contains the left-bottom corner 
+        # of 'k', then all the cells covered by the circuit 'k' must contain only that circuit and no other circuits.      
+        for k in range(n):
+            for i in range(w-w_min+1):
+                for j in range(l_max-h_min+1):
+                    # Current iteration: circuit `k` and cell `(i,j)` of the plate.
+                    # Now the constraint about putting the left-bottom corner of `k` in `(i,j)` is ensured.
+
+                    # Case in which `k` can't be put in `(i,j)`, because it goes out of the bounds of the plate.
+                    # In such case, a constraint ensuring that `k` can't be put in `(i,j)` is esnured.
+                    if i+dimsX[k]-1>=w or j+dimsY[k]-1>=l_max:
+                        s.add(Not(coords[i][j][k]))  # The left-bottom corner of `k` can't be put in `(i,j)`
+                        continue
+                    
+                    # List of tuples, representing the coordinates of the cells of the plate covered by the circuit
+                    covered_positions = [(i+ii,j+jj) for ii in range(dimsX[k]) for jj in range(dimsY[k])
+                                                    if i+ii<w-w_min+1 and j+jj<l_max-h_min+1]
+
+                    # Formula ensuring that no other circuit is present in the `covered_positions`
+                    no_overlapping_circuit_formula = And([Not(circuits[ii][jj][kk]) 
+                                                        for (ii,jj) in covered_positions for kk in range(n) if kk!=k])
+                    # Formula ensuring that all the `covered_positions` actually contain that circuit `k`
+                    all_positions_covered_formula = And([circuits[ii][jj][k] for (ii,jj) in covered_positions])
+
+                    # TODO: put negation of all circuits[ii][jj][k] related to wrong positions? 
+                    # The added constraint is the following implication: if left-bottom corner of `k` in `(i,j)`, then 
+                    # `no_overlapping_circuit_formula` and `all_positions_covered_formula`.
+                    # Actually, it is not an implication, but an equivalence.
+                    s.add(coords[i][j][k] == And(no_overlapping_circuit_formula,all_positions_covered_formula))
+
+                    # Formula ensuring that all the lengths up to `j+dimsY[k]-1` are used by the circuit `k`
+                    used_lengths_formula = And([lengths[k][l] for l in range(j+dimsY[k]-l_min+1)])
+                    # Formula ensuring that all the lengths from `j+dimsY[k]` are not used by the circuit `k`
+                    non_used_lengths_formula = And([Not(lengths[k][l]) for l in range(j+dimsY[k]-l_min+1, l_max-l_min+1)])
+                    # The added constraint is the following implication: if left-bottom corner of `k` in `(i,j)`, then 
+                    # `used_lengths_formula` and `non_used_lengths_formula`.
+                    s.add(Implies(coords[i][j][k], And(used_lengths_formula, non_used_lengths_formula)))
+
+        # print('HERE')  # TODO: remove
+
+        # Check if UNSAT 
+        # if s.check() != sat:
+        #    raise UnsatError('UNSAT')
+            
+        return s, coords, lengths
+
+
+    def __process_solver_instance(self, s, lb, ub, coords, w_min, h_min, l_max):
+        res = s.check()==sat
+        if res:
+            self.results['best_coords'] = self.__compute_result(s, coords, w_min, h_min, l_max)
+            self.results['best_l'] = lb
+            if lb+1==ub:
+                ub = lb
+            else:
+                ub = math.ceil((lb+ub)/2)
+        else:
+            s.pop()
+            lb = lb+1
+        return res, lb, ub
+
+    
+    def __inject(self, s, lengths, lb, ub, l_min, l_max):
+        n = self.n
+
+        med = math.ceil((lb+ub)/2)
+
+        lb_index = lb-l_min
+        ub_index = ub-l_min
+        med_index = med-l_min
+
+        s.add(And([Not(lengths[k][ll]) for k in range(n) for ll in range(lb_index+1,l_max-l_min)]))
+
+
+    def __compute_result(self, s, coords, w_min, h_min, l_max):
+        w, n = self.w, self.n
+        
+        # Get the solution
+        m = s.model()
+
+        # List containing the coordX and coordY of the lower-left vertex of each circuit in the new solution
+        coords_sol = [(i, j) for k in range(n) for j in range(l_max-h_min+1) for i in range(w-w_min+1) if m.evaluate(coords[i][j][k])]
+
+        # Length of the plate in the new solution
+        # l = max([l for k in range(n) for l in range(l_max-l_min+1) if m.evaluate(lengths[k][l])])+l_min-1+1
+
+        return coords_sol#, l
+
+
+
+    def __optimize(self):
+        """Solves the given VLSI instance, using the SAT encoding 4A.
+
+        It performs optimization: the best solution is found (if any).
+        (If this class is used as a parallel process with a time limit, there is not gurantee of founding the optimal 
+        solution, but only the best solution found so far).
+
+        The implementation is based on the usage of the `__solve` method.
+        The solver is created only one time, at the beginning. Then, a cycle is performed, in which at each iteration 
+        new constraints are injected into the solver, namely: the constraints imposing that the already found solutions are not 
+        feasible anymore (like in the previous encodings); the constraints imposing that the lengths of the plate bigger than 
+        the current best length are not feasible anymore (this is done using the 'length_k_l' variables).
+
+        Incremental solving.
+
+        Raises
+        ------
+        UnsatError
+            If the given instance is UNSAT
+
+        Notes
+        ------
+        The solution is communicated to the user through the `results` dictionary, which is shared between the class and the 
+        user. 
+        Each time a better solution is found, it is injected into the `results` dictionary.
+        """
+        w, n, dimsX, dimsY = self.w, self.n, self.dimsX, self.dimsY
+
+        areas = [dimsX[i]*dimsY[i] for i in range(n)]  # The areas of the circuits
+        A_tot = sum(areas)  # The overall area of all the given circuits
+        h_min = min(dimsY)  # The minimum height of a circuit
+        h_max = max(dimsY)  # The maximum height of a circuit
+        w_min = min(dimsX)  # The minimum width of a circuit
+        w_max = max(dimsX)  # The maximum width of a circuit
+        l_min = max([h_max, A_tot // w])  # The lower bound for the length
+        min_rects_per_row = w // w_max  # Minimum number of circuits per row
+        # max_rects_per_col = ceil(n / max([1, min_rects_per_row]))  # Maximum number of circuits per column
+        #l_max =  sum(sorted(dimsY)[n-max_rects_per_col:]) 
+        if min_rects_per_row==0:
+            raise UnsatError('UNSAT')
+        sorted_dimsY = sorted(dimsY, reverse=True)  
+        if min_rects_per_row==0:
+            l_max = sum(dimsY)
+        else:
+            l_max = sum([sorted_dimsY[i] for i in range(n) if i % min_rects_per_row == 0])  # The upper bound for the length
+
+        # Search for a first solution 
+        s, coords, lengths = self.__solve(w_min, h_min, l_min, l_max)
+        s.push()
+        lb = l_min
+        ub = l_max
+        self.__inject(s, lengths, lb, ub, l_min, l_max)
+
+        res = False
+
+        #self.results['best_coords'], self.results['best_l'] = self.__process_solver_instance(s, coords, lengths, w_min, 
+         #                                                                                    h_min, l_min, l_max, 
+          #                                                                                   current_best_l=None)
+
+        # A first solution has been found
+
+        # print(self.results['best_coords'])
+        # print(self.results['best_l'])
+        
+        # Loop iterating over all the possible solutions, searching for the best one
+        while not res and lb<ub:
+            print(lb, ub)
+            #if s.check()!=sat:  # No more solutions: break the cycle
+            #    break
+            res, lb, ub = self.__process_solver_instance(s, lb, ub, coords, w_min, h_min, l_max)
+            print(res, lb, ub)
+            s.push()
+            self.__inject(s, lengths, lb, ub, l_min, l_max)
+
+
+            # A new solution has been found
+
+            # Get the new valid solution and inject the new constraints into the solver
+            #self.results['best_coords'], self.results['best_l'] = self.__process_solver_instance(s, coords, lengths, w_min, 
+             #                                                                                    h_min, l_min, l_max, 
+              #                                                                                   self.results['best_l'])        
+            # print(self.results['best_coords'])
+            # print(self.results['best_l'])
+        
+
+        if not res:
+            raise UnsatError('UNSAT')
+
+        print('HHH')
+
+        self.results['finish'] = True      
+
+
+    def run(self):
+        # Code executed by the process when it is runned
+        try:
+            self.__optimize()
+        except UnsatError:
+            self.results['unsat'] = True
