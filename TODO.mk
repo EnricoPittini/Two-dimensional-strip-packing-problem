7ac85d18df6db15e2237f33533e9e7e56afbebb2
Enrico Pittini <pittinienrico@hotmail.it>
2022-06-18 13:15:47 +0200

    Update TODO.mk

--- TODO.mk
+++ TODO.mk
@@ -19,18 +19,7 @@
 
 # SAT TODOs
 - Fix in-file TODOs 
-- Refactor and write better encoding 3, in order to fix it (write functions to access the indeces of the length_k_l variables)
-- Think about the constraints. In particular, think about the equivalence, about the implications, about the 'redundant' constraints
-- Sliding window constraints
-- Implement encoding found in the pdf
-- Try binary search for optimization
-- Try other encodings of at most one (see the paper)
-- Put simmetry breaking constraints
-- Add timeout info (timeout/not timeout)
-- Improve timeout
-- Put the 'run()' method in the superclass
 - Delete instance 41
-- Fix bounds of the variables in the comments (e.g. w-w_min)
 - Fix name exactly_one for coords (name=f'exactly_one_coord_{k}')
 
 - Try refactor and use timeout of the Z3 solver (instead of using a Thread)
