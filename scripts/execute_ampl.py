162a32328bd1b0f2b48e81fd65a8ce7ea2ea8fe5
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2022-06-26 10:25:48 +0200

    LP MiniZinc models; `execute_ampl` script refactor

--- scripts/execute_ampl.py
+++ scripts/execute_ampl.py
@@ -1,12 +1,8 @@
-import argparse
-import json
-import os
-import re
-import subprocess
-import sys
 from amplpy import AMPL, AMPLException
-import tempfile
-import pandas
+import argparse
+import os
+import sys
+import time
 
 import utils
 
@@ -21,7 +17,8 @@ def main() -> None:
 
     parser.add_argument('instance-path', type=argparse.FileType('r', encoding='UTF-8'), help='The instance to solve.')
 
-    parser.add_argument('solver-name', type=str, choices=_SOLVER_CHOICES, help='The solver used for optimization.')
+    parser.add_argument('solver-name', type=str, default='cplex', nargs='?', choices=_SOLVER_CHOICES, 
+                        help='The solver used for optimization.')
 
     parser.add_argument('output-folder-path', type=str, default=os.getcwd(), nargs='?', 
                         help='The path in which the output file is stored.')
@@ -30,7 +27,7 @@ def main() -> None:
                         help='The name of the output solution.')
 
     parser.add_argument('--time-limit', '-t', type=int, default=300, nargs='?',
-                        help='The allowed time to solve the task.', required=False)
+                        help='The allowed time to solve the task in seconds.', required=False)
 
     parser.add_argument('--no-create-output', action='store_true', 
                         help='Skip the creation of the output solution.')
@@ -38,65 +35,61 @@ def main() -> None:
     parser.add_argument('--no-visualize-output', action='store_true', 
                         help='Skip the visualization of the output solution (defaults as true if --no-create-output ' + \
                         'is passed).')
-    
-    #parser.add_argument('--symmetry-breaking-option', '-sbo', nargs='?',  type=int,
-    #                    help='The symmetry breaking options to use for the MiniZinc model.', default=None)
 
     arguments = parser.parse_args()
 
+    model_path = vars(arguments)["model-path"]
     instance_file = vars(arguments)['instance-path']
+    solver = vars(arguments)['solver-name']
+    time_limit = arguments.time_limit
+    
     w, n, dims = utils.parse_instance_txt(instance_file)
     
-    time_limit = arguments.time_limit
-    #TODO remove
-    time_limit = 300
-
-    solver = vars(arguments)['solver-name']
-
-
-
-
     try:
         ampl = AMPL()
         ampl.set_option('solver', solver)
+        
+        # Set solver dependent time limit option.
         if solver == 'cplex':
             ampl.set_option('cplex_options',f"timelimit={time_limit}")
-
         elif solver == 'cbc':
             ampl.set_option('cbc_options',f"sec={time_limit}")
         elif solver == 'gurobi':
             ampl.set_option('gurobi_options',f"timelim={time_limit}")
+        else:
+            parser.error(f"argument solver-name: invalid choice: '{solver}' " + 
+                         f"(choose from {', '.join([f'{s}' for s in _SOLVER_CHOICES])})")
 
-        ampl.read(os.path.normpath(vars(arguments)["model-path"]))
+        # Read model and data files.
+        ampl.read(os.path.normpath(model_path))
         _read_dat_file(w, n, dims, ampl)
-        #print(ampl.get_value('solve_result'))
-        # Solve
-        #TODO start and stop timer
         
+        start_time = time.time()
         ampl.solve()
-
-        #TODO time refactor
+        solving_time = time.time() - start_time
         
+        # Parse optimization result status.
         result = ampl.get_value('solve_result')
         if result == 'infeasible':
             sys.exit('ERROR: UNSATISFIABLE')
         elif result == 'limit':
             print('% Time limit exceeded!')
         elif result == 'solved':
-            print(ampl.get_value('_total_solve_time'))
+            print(f'% Time elapsed {solving_time}')
         else:
             sys.exit('ERROR: UNKOWN')
 
-
-        l = ampl.get_value('l')
+        # Parse variables results.
+        l = int(ampl.get_value('l'))
+        
         coordsX = ampl.get_data('coordsX').to_pandas().coordsX.values
         coordsY = ampl.get_data('coordsY').to_pandas().coordsY.values
         coordsX = coordsX.astype(int)
         coordsY = coordsY.astype(int)
-        l = int(l)
-        print(f'l={l}')
-        print(f'CoordsX={coordsX}')
-        print(f'CoordsY={coordsY}')
+
+        print(f'l = {l}')
+        print(f'CoordsX = {coordsX}')
+        print(f'CoordsY = {coordsY}')
     except AMPLException:
         sys.exit('ERROR: UNKNOWN')
 
@@ -123,10 +116,10 @@ def main() -> None:
             os.system(f'python {visualize_script_path} "{output_file}"')
 
 def _read_dat_file(w, n, dims, ampl):         
+    data_file_path = 'data.dat'
     
-    #TODO delete file
-    
-    with open('data.dat','w') as fp:
+    # Create `.dat` data file with `AMPL` required format.
+    with open(data_file_path,'w') as fp:
         fp.write('data;\n')
         fp.write(f'param n := {n};\n')
         fp.write(f'param w := {w};\n')
@@ -134,7 +127,12 @@ def _read_dat_file(w, n, dims, ampl):
         for i, dim in enumerate(dims):
             fp.write(f'{i+1}\t\t{dim[0]}\t\t{dim[1]}\n')
         fp.write(';')
-    ampl.read_data('data.dat')
+    
+    ampl.read_data(data_file_path)
+    
+    # Delete `.dat` data file.
+    if os.path.exists(data_file_path):
+        os.remove(data_file_path)
     
 if __name__ == '__main__':
     main()
