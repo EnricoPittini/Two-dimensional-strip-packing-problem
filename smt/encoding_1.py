4cdfff34ca77b5e60913594cb1baf85ca0066831
Enrico Pittini <pittinienrico@hotmail.it>
2022-06-27 15:33:37 +0200

    Refactor and comment encoding 1

--- smt/encoding_1.py
+++ smt/encoding_1.py
@@ -3,17 +3,49 @@ from smt_utils import Vlsi_smt_abstract
 import subprocess
 
 class Vlsi_smt(Vlsi_smt_abstract):
+    """Class for solving the VLSI problem in SMT, using the encoding 1.
+
+    It inherits from the class `Vlsi_smt_abstract`.
+
+    Same solving and optimization of the encoding 0.
+    The only difference is that a better initial upper bound for the length of the plate (i.e. `l_max`) has been used.
+
+    """
     def __generate_encoding(self, l_max):
+        """Generates the SMT encoding for the specific instance of the VLSI problem according to the encoding 1.
+
+        The SMT encoding is generated into a SMT-LIB file, with name "encoding_1_{instance_name}.smt2".
+
+        Parameters
+        ----------
+        l_max : int
+            Maximum length of the plate.
+
+        Returns
+        -------
+        specificInstance_encoding_abspath : str
+            Absolute path of the SMT encoding file for the specific instance.
+
+        Notes
+        ------
+        The following SMT variables are used.
+        - coordX[i], where 'i' in [0,n-1].
+          coordX[i] is the x coordinate of the bottom-left vertex of the 'i'-th circuit.
+        - coordY[i], where 'i' in [0,n-1].
+          coordY[i] is the y coordinate of the bottom-left vertex of the 'i'-th circuit.
+          
+        """
         instance_name, w, n, dimsX, dimsY = self.instance_name, self.w, self.n, self.dimsX, self.dimsY
 
         lines = []  # List of lines to write in the SMT-LIB file. It is a list of strings.
 
+        # For getting the model
         lines.append('(set-option :produce-models true)')
 
         # VARIABLES
-        # We are defining the function "coordX". We are declaring n variables "coordX[i]".
+        # We are defining the function "coordX": we are declaring n variables "coordX[i]".
         lines.append('(declare-fun coordX (Int) Int)')
-        # We are defining the function "coordY". We are declaring n variables "coordY[i]".
+        # We are defining the function "coordY": we are declaring n variables "coordY[i]".
         lines.append('(declare-fun coordY (Int) Int)')
 
         # CONSTRAINTS
@@ -30,7 +62,9 @@ class Vlsi_smt(Vlsi_smt_abstract):
         lines += [f'(assert (and (>= (coordY {i}) 0) (<= (coordY {i}) (- {l_max} {dimsY[i]}))))' for i in range(n)]
 
         # 2- Non-overlapping
-        # For each pair of circuits i-j, we impose the non-overlapping constraint
+        # For each pair of circuits (i,j), where i<j, we impose the non-overlapping constraint: 
+        #            coordX[i]+dimsX[i]<=coordX[j] \/ coordX[j]+dimsX[j]<=coordX[i] \/ coordY[i]+dimsY[i]<=coordY[j] \/ 
+        #                                             coordY[j]+dimsY[j]<=coordY[i]
         lines += [f'(assert (or (<= (+ (coordX {i}) {dimsX[i]}) (coordX {j})) (<= (+ (coordX {j}) {dimsX[j]}) (coordX {i})) (<= (+ (coordY {i}) {dimsY[i]}) (coordY {j})) (<= (+ (coordY {j}) {dimsY[j]}) (coordY {i}))))' 
                 for i in range(n) for j in range(n) if i<j]
 
@@ -40,14 +74,16 @@ class Vlsi_smt(Vlsi_smt_abstract):
         lines.append(f'(get-value ({" ".join([f"(coordX {i}) (coordY {i}) " for i in range(n)])}))')
 
         # FINALLY, CREATING THE FILE AND WRITING THE LINES
+        # Absolute path of the current directory
         curr_abs_path = os.path.dirname(os.path.abspath(__file__))
-        # print(curr_abs_path)
-        specific_instance_abspath = os.path.join(curr_abs_path, f'encoding_1_{instance_name}.smt2')
-        with open(specific_instance_abspath, 'w') as f:
+        # Absolute path of the SMT-LIB file containing the encoding of this specific VLSI instance
+        specificInstance_encoding_abspath = os.path.join(curr_abs_path, f'encoding_1_{instance_name}.smt2')
+        # Writing the lines into the file
+        with open(specificInstance_encoding_abspath, 'w') as f:
             for line in lines:
                 f.write(line + '\n')
 
-        return specific_instance_abspath
+        return specificInstance_encoding_abspath
 
 
     def __compute_l(self, coords):
@@ -68,29 +104,29 @@ class Vlsi_smt(Vlsi_smt_abstract):
 
 
     def __optimize(self):
-        """Solves the given VLSI instance, using the SAT encoding 0.
+        """Solves the given VLSI instance, using the SMT encoding 1.
 
         It performs optimization: the best solution is found (if any).
         (If this class is used as a parallel process with a time limit, there is not gurantee of founding the optimal 
-        solution, but only the best solution found so far).
+        solution, but only the best solution found within the time limit).
 
-        The implementation is based on the usage of the `__solve` method.
-        Basically, a loop iterating over all the possible solutions is performed, searching for the best possible solution.
-        At each iteration, the solver is created and run from scratch, with additional constraints imposing to search 
-        for a solution different from the ones already found (the already found solutions are not feasible anymore).
+        The implementation is based on the usage of the `__generate_encoding` method.
+
+        The optimization procedure is the following. Cycle, in which at each iteration the encoding is generated from scratch
+        and the solver is created and run from scratch on that encoding. At each iteration, the current best length of the 
+        plate (i.e. `l`) is given to the solver as upper bound for the length of the plate (i.e. `l_max`). (Actually, `l-1` 
+        is given as `l_max`).
+        In this way, at each iteration is found a better solution with respect to the current one. 
+        We stop the cycle when we find UNSAT.
 
         No incremental solving: at each iteration, the solver is created and run from scratch.
-
-        Raises
-        ------
-        UnsatError
-            If the given instance is UNSAT
 
         Notes
         ------
         The solution is communicated to the user through the `results` dictionary, which is shared between the class and the 
         user. 
         Each time a better solution is found, it is injected to the `results` dictionary.
+
         """
         solver_name = self.solver_name
 
@@ -99,9 +135,10 @@ class Vlsi_smt(Vlsi_smt_abstract):
         # Boolean flag reprenting if a first solution has already been found
         first_solution = False
 
+        # Computing the upper bound of the length of the plate (i.e. `l_max`)
         w_max = max(dimsX)  # The maximum width of a circuit
         min_rects_per_row = w // w_max  # Minimum number of circuits per row
-        if min_rects_per_row==0:
+        if min_rects_per_row==0:  # UNSAT
             self.results['finish'] = True
             self.results['unsat'] = True  
             return
@@ -112,27 +149,31 @@ class Vlsi_smt(Vlsi_smt_abstract):
             l_max = sum([sorted_dimsY[i] for i in range(n) if i % min_rects_per_row == 0])  # The upper bound for the length
 
         while True:           
-            # Search for a solution, given the additional constraints in `formulas` and given the current l_max
+            # Generate the SMT encoding, into a SMT-LIB file
             specific_encoding_path = self.__generate_encoding(l_max=l_max)
-            command = f'{solver_name} "{specific_encoding_path}"'
 
+            # Run the solver on the generated SMT-LIB encoding
+            command = f'{solver_name} "{specific_encoding_path}"'
             result = subprocess.run(command, capture_output=True)
 
+            # Delete the SMT-LIB file containing the encoding
             os.remove(specific_encoding_path)
 
+            # Get the output of the solver
             output = result.stdout.decode('UTF-8')
-            #print(output)
 
+            # UNSAT: break the cycle
             if 'unsat' in output:
                 break
 
             # SAT: a new solution has been found
             
+            # Parse the output of the solver into the list of coords, i.e. `coords`
             if solver_name=='z3':
                 coords = [int(s.split(')')[1]) for s in output.split('\n')[1:-1]]
             elif solver_name=='cvc5':
                 coords = [int(s.split(')')[1]) for s in output.split('\n')[1:-1][0].split(' (')]
-            #print(coords)
+            # List of coords, for each circuit 'i'. For each circuit 'i', we have the list of two elements [coordX_i, coordY_i]
             coords = [[coords[2*i], coords[2*i+1]] for i in range((len(coords))//2)]
  
             # Length of the plate of the current solution
@@ -157,5 +198,5 @@ class Vlsi_smt(Vlsi_smt_abstract):
 
 
     def run(self):
-        # Code executed by the process when it is runned
+        # Code executed by the process when it is run
         self.__optimize()
