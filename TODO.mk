a70c6b9feb633cd85db94bdc6f6063c807d926b8
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2022-07-02 13:14:42 +0200

    Refactor cp heuristics + other refactor

--- TODO.mk
+++ TODO.mk
@@ -1,22 +1,6 @@
 # Python script TODOs
-- Static types refactoring
-- Try-except handling
 - Already existing output file handling
 - Check and clean visualization script
-- Handle platform path independency
-- Solver parameters through script
-- Automatic instance and model path files retrival
-- Delete time selection from solver config and put it directly in minizinc call in execute_cp.py
-
-# MiniZinc model TODOs
-- Heuristic
-- Remove 'dims' and keep only 'dimsX, dimsY'
-- Set refactoring
-- For the final model try Graph vs Bound consistency
-- Test "gecode" (restarting "dom_w_deg" "LNS")
-
-# Generic TODOs
-- Possible solve perfect model
 
 # SAT TODOs
 - Fix in-file TODOs 
--- scripts/utils.py
+++ scripts/utils.py
@@ -3,10 +3,11 @@ import os
 
 
 MINIZINC_ERRORS = ['UNSATISFIABLE', 'UNBOUNDED', 'UNSATorUNBOUNDED', 'UNKNOWN', 'ERROR']
-GECODE_MODELS = [f'model_6{i}1' for i in 'BCDE'] + ['model_6F']
+GECODE_MODELS = ['model_6F']
 # TODO: Fix 
 CHUFFED_MODELS = [f'model_{i}' for i in range(3)] + [f'model_3{i}' for i in 'ABC'] + [f'model_4A{i}' for i in range(8)] +\
-    [f'model_4{i}{j}' for i in 'BC' for j in range(3)] + ['model_5', 'model_6A'] + [f'model_6{i}0' for i in 'BCDE']
+    [f'model_4{i}{j}' for i in 'BC' for j in range(3)] + ['model_5'] + [f'model_6{i}' for i in 'ABC'] +\
+    [f'model_6{i}{j}' for i in 'DE' for j in range(2)] #TODO change
 MINIZINC_MODELS = GECODE_MODELS + CHUFFED_MODELS + ['model_final']
 AMPL_SOLVER_CHOICES = ['cbc','cplex','gurobi']
 AMPL_MODEL_CHOICES = ['model_0','model_1', 'model_2A', 'model_2B','model_dual']
